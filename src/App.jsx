e1a69e847d541e6ebc03a84364ec02e5b8e9ebe6
Stas <stassmirnov115@gmail.com>
2022-12-07 16:23:14 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.scss";
+import logo from "./assets/images/logo.png";
+import user from "./assets/images/icons/user.svg";
+import cart from "./assets/images/icons/cart.svg";
+import product from "./assets/images/product/product-1.jpg";
+import btnAdd from "./assets/images/icons/button-add.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="wrapper">
+      <div className="container">
+        <header className="header">
+          <div className="header__logo">
+            <img src={logo} width={40} height={40} alt="logo" />
+            <div>
+              <h1 className="header__title">REACT Shop</h1>
+              <p className="header__subtitle">Магазин лучших кроссовок</p>
+            </div>
+          </div>
+          <ul className="header__right" role="list">
+            <li className="header__cart">
+              <img src={cart} alt="cart" />
+              <span>12 999 руб.</span>
+            </li>
+            <li className="header__user">
+              <img src={user} alt="user" />
+            </li>
+          </ul>
+        </header>
+        <main className="main">
+          <h2 className="main__title">Все кроссовки</h2>
+          <ul className="main__products" role="list">
+            <li className="main__product">
+              <img className="main__img" src={product} alt="product" />
+              <div className="main__name">
+                <span>Мужские Кроссовки</span>
+                <span>Nike Brazer Mid Suede</span>
+              </div>
+              <div className="main__box">
+                <div className="main__price">
+                  <span>Цена: </span>
+                  <span>12 999 руб.</span>
+                </div>
+                <button className="main__button">
+                  <img src={btnAdd} alt="" />
+                </button>
+              </div>
+            </li>
+          </ul>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
