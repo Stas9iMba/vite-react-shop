358a0142df03c52a93adeb2fc3a71e2be47da130
Stas <stassmirnov115@gmail.com>
2022-12-18 00:56:50 +0700

    Search input

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import "./App.scss";
 function App() {
   const [items, setItems] = React.useState([]);
   const [cardItems, setCardItems] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState("");
   const [isOpenedCard, setIsOpenedCard] = React.useState(false);
 
   function onAddToCard(obj) {
@@ -21,6 +22,14 @@ function App() {
       .then((json) => setItems(json));
   }, []);
 
+  function onChangeSearchInput(event) {
+    setSearchValue(event.target.value);
+  }
+
+  function removeSearchInput() {
+    setSearchValue("");
+  }
+
   return (
     <div className="wrapper">
       {isOpenedCard && (
@@ -29,7 +38,9 @@ function App() {
       <Header onClickOpened={() => setIsOpenedCard(true)} />
       <main className="main">
         <div className="main__top">
-          <h2 className="main__title">Все кроссовки</h2>
+          <h2 className="main__title">
+            {searchValue ? `Поиск: "${searchValue}"` : "Все кроссовки"}
+          </h2>
           <form className="main__form" action="#">
             <img
               className="search"
@@ -38,19 +49,39 @@ function App() {
               src="/assets/images/icons/search.svg"
               alt="search"
             />
-            <input type="text" placeholder="&nbsp;" required />
+            <input
+              onChange={onChangeSearchInput}
+              value={searchValue}
+              type="text"
+              placeholder="&nbsp;"
+              required
+            />
+            {searchValue && (
+              <img
+                className="remove"
+                width={15}
+                height={15}
+                src="/assets/images/icons/button-remove.svg"
+                alt="remove"
+                onClick={removeSearchInput}
+              />
+            )}
           </form>
         </div>
         <ul className="main__products" role="list">
-          {items.map((obj, index) => {
-            return (
-              <Card
-                {...obj}
-                key={index}
-                addProduct={(item) => onAddToCard(item)}
-              />
-            );
-          })}
+          {items
+            .filter((item) =>
+              item.name.toLowerCase().includes(searchValue.toLowerCase(), [])
+            )
+            .map((obj, index) => {
+              return (
+                <Card
+                  {...obj}
+                  key={index}
+                  addProduct={(item) => onAddToCard(item)}
+                />
+              );
+            })}
         </ul>
       </main>
     </div>
